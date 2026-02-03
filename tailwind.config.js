407f74cc389e8e6c26c232ba133e54ae792f2270
Jacob <535707+jkub@users.noreply.github.com>
2026-02-02 21:23:50 -0800

    wtf

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./constants.tsx",
+    "./index.tsx"
   ],
   theme: {
     extend: {},
