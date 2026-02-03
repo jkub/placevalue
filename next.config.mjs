1bd722d86b83306547329243cb951db73395039a
Jacob <535707+jkub@users.noreply.github.com>
2026-02-02 22:18:55 -0800

    ?

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  /**
-   * IMPORTANT FOR GITHUB PAGES:
-   * If your site is at https://<username>.github.io/<repo-name>/
-   * you MUST uncomment the line below and set it to your repository name.
-   */
   basePath: '/placevalue',
+  trailingSlash: true,
   reactStrictMode: true,
 };
 
